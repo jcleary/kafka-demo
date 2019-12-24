58bffc17e02b94ad0f5f9238b8a9fd7dc38b20ea
John Cleary <john@createk.io>
2019-12-24 19:08:54 +0000

    add party topic and send messages on save

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,2 +1,10 @@
 class Party < ApplicationRecord
+  after_save :send_change_nessage
+
+
+  private
+
+  def send_change_nessage
+    WaterDrop::SyncProducer.call(self.to_json, topic: 'party')
+  end
 end
--- config/initializers/water_drop.rb
+++ config/initializers/water_drop.rb
@@ -1,10 +1,14 @@
-class App < Karafka::App
-  setup do |config|
-    config.kafka.seed_brokers = ['kafka://kafka:9092']
-    config.client_id = "client_#{ENV['APP_ID']}"
-  end
-end
+WaterDrop.setup do |config|
+  config.deliver = true
+  config.kafka.seed_brokers = %w[kafka://kafka:9092]
+end  
+# setup do |config|
+#     config.kafka.seed_brokers = ['kafka://kafka:9092']
+#     config.client_id = "client_#{ENV['APP_ID']}"
+#     # config.backend = :sidekiq
+#   end
+# end
 
-App.boot!
+# App.boot!
 
 
