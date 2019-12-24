361ca87bc4f21a19387cef3081cb83121bceee4e
John Cleary <john@createk.io>
2019-12-24 16:04:12 +0000

    add create action for party, creates a new Party with some fake attributes

--- app/controllers/party_controller.rb
+++ app/controllers/party_controller.rb
@@ -2,4 +2,14 @@ class PartyController < ApplicationController
   def index
     @parties = Party.all
   end
+
+  def create
+    Party.create(
+      uid: SecureRandom.uuid,
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      date_of_birth: Faker::Date.birthday(18, 65)
+    )
+    redirect_to action: :index
+  end
 end
