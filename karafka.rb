df9bbd5ee9cc99266766fe823d611cfc07bfb2ec
John Cleary <john@createk.io>
2019-12-29 13:10:08 +0000

    Karafka config

---
+++ karafka.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+ENV['RAILS_ENV'] ||= 'development'
+ENV['KARAFKA_ENV'] = ENV['RAILS_ENV']
+require ::File.expand_path('../config/environment', __FILE__)
+Rails.application.eager_load!
+
+# This lines will make Karafka print to stdout like puma or unicorn
+if Rails.env.development?
+  Rails.logger.extend(
+    ActiveSupport::Logger.broadcast(
+      ActiveSupport::Logger.new($stdout)
+    )
+  )
+end
+
+class KarafkaApp < Karafka::App
+  setup do |config|
+    config.kafka.seed_brokers = %w[kafka://kafka:9092]
+    config.client_id = "app_#{ ENV['APP_ID'] }"
+    config.logger = Rails.logger
+  end
+
+  # Comment out this part if you are not using instrumentation and/or you are not
+  # interested in logging events for certain environments. Since instrumentation
+  # notifications add extra boilerplate, if you want to achieve max performance,
+  # listen to only what you really need for given environment.
+  Karafka.monitor.subscribe(WaterDrop::Instrumentation::StdoutListener.new)
+  Karafka.monitor.subscribe(Karafka::Instrumentation::StdoutListener.new)
+  Karafka.monitor.subscribe(Karafka::Instrumentation::ProctitleListener.new)
+
+  consumer_groups.draw do
+    topic :party do
+      consumer PartyConsumer
+      batch_consuming false
+    end
+
+    # topic :example do
+    #   consumer ExampleConsumer
+    # end
+
+    # consumer_group :bigger_group do
+    #   topic :test do
+    #     consumer TestConsumer
+    #   end
+    #
+    #   topic :test2 do
+    #     consumer Test2Consumer
+    #   end
+    # end
+  end
+end
+
+Karafka.monitor.subscribe('app.initialized') do
+  # Put here all the things you want to do after the Karafka framework
+  # initialization
+end
+
+KarafkaApp.boot!
