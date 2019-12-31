de5164a6c92c7b88440f287c97ca5b5851cfdff5
John Cleary <john@createk.io>
2019-12-31 18:21:10 +0000

    don't remove kafka volume on restart

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker-compose kill 
-docker-compose rm -v -f manager kafka zookeeper manager karafka_server_1 karafka_server_2 karafka_server_3 web_1 web_2 web_3 
+docker-compose rm -v -f manager zookeeper manager karafka_server_1 karafka_server_2 karafka_server_3 web_1 web_2 web_3 
 docker-compose up
